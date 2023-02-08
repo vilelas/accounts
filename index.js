a470ddca3a211bc273507ef167181277539a017b
João Santos <118611278+vilelas@users.noreply.github.com>
2023-02-08 11:15:06 -0300

    criação da função principal

--- index.js
+++ index.js
@@ -4,3 +4,28 @@ const inquirer = require("inquirer");
 
 // módulos internos
 const fs = require("fs");
+
+const operation = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "O que você deseja fazer?",
+        choices: [
+          "Criar conta",
+          "Consultar saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer["action"];
+      console.log(action);
+    })
+    .catch((err) => console.log(err));
+};
+
+operation();
